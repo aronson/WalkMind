5581cf04d676ff85e87ed5ef39ac016a096a9361
aronson <i@pingas.org>
2023-06-29 19:39:28 -0500

    Add DumpMind project

---
+++ src/DumpMind/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open FSharp.Json
+open NHotkey
+open NHotkey.WindowsForms
+open System.Windows.Forms
+open System.Windows.Input
+open WalkMind.Memory
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv =
+    let memory = Memory()
+    let getPlayerString() = Option.get memory.player.entity |> Json.serialize
+    let getEnemyString() =
+        match memory.tiles.[memory.mapCursorIndex].entity with
+        | None -> None
+        | Some entity ->
+            Some <| Json.serialize entity
+            
+    let getStrings() =
+        let player = getPlayerString()
+        let enemy = getEnemyString()
+        match enemy with
+        | None -> sprintf "%s" player
+        | Some enemy -> sprintf "[\n%s,\n%s\n]" player enemy
+    
+    let hotkeyManager = HotkeyManager.Current
+    
+    hotkeyManager.AddOrReplace("DumpGameData", Keys.F11 ||| Keys.Control, (fun _ _ -> Clipboard.SetText(getStrings())))
+    printfn "Press Ctrl+F11 with an enemy highlighted to dump to console...\n"
+
+    // Enter message loop
+    Application.Run()
+    0 // return an integer exit code
